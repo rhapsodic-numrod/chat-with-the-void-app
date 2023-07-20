9ac54d88ccb9bf89cf02ab42be80495e843d8f70
Profiliu <muaprof2@gmail.com>
2023-07-20 15:26:57 +0200

    removed user

--- lib/pages/auth_switcher.dart
+++ lib/pages/auth_switcher.dart
@@ -9,7 +9,6 @@ class AuthSwitcher extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final user = FirebaseAuth.instance.currentUser;
 
     return Scaffold(
       body: StreamBuilder<User?>(
