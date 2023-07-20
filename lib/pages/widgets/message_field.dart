e75fb2fd149c525fa519a459da803a7f8aa57910
Profiliu <muaprof2@gmail.com>
2023-07-20 15:27:07 +0200

    removed unused imports

--- lib/pages/widgets/message_field.dart
+++ lib/pages/widgets/message_field.dart
@@ -1,8 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:test_firebase_chat_app2/app_state.dart';
 
 class MessageField extends StatefulWidget {
   const MessageField({
